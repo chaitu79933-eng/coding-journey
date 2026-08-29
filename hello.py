726ff757be175cb70824a078e3ddcd3dc5bd874b
chaitanya <chaitu79933@gmail.com>
2026-08-29 19:05:43 +0530

    start python coding journey

---
+++ hello.py
@@ -0,0 +1,17 @@
+name = "chaitu" 
+age = 18 
+print(name)
+print(age)
+print("im learning python!")
+a = 10
+b = 5
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+name = input("what's your name?")
+print("hello", name)
+age = input("what's your age?")
+print("age:", age)
+
+
